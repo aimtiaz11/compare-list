a36e08212dc5a91b930fd58bd12ab719133211f5
Ali Imtiaz <alimtiaz@deloitte.com.au>
2018-11-02 10:56:42 +1100

    Added sample output to readme

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # compare-list
 
-Simple Groovy script to compare 2 files. 
+SimpleGroovyscripttocompare2files.
 
 
 ## Running
-Populate the input-file1.txt and input-file2.txt and run: `groovy compareList.groovy`
+Populatetheinput-file1.txtandinput-file2.txtandrun:`groovycompareList.groovy`
 
+## Sample output
+
+
+```
+Total in sourceList: 3
+Total in targetList: 3
+**** Items in sourceList not in targetList list: 2 ****
+1
+
+2
+
+**** Items in targetList not in sourceList list: 2 ****
+4
+
+5
+
+***** common items *****
+3
+````
--- compareList.groovy
+++ compareList.groovy
@@ -1,29 +1,29 @@
-def sourceList = []
-new File( 'input-file1.txt' ).eachLine { line ->
-    sourceList << line
+defsourceList=[]
+newFile('input-file1.txt').eachLine{line->
+sourceList<<line
 }
 
-def targetList = []
-new File( 'input-file2.txt' ).eachLine { line ->
-    targetList << line
+deftargetList=[]
+newFile('input-file2.txt').eachLine{line->
+targetList<<line
 }
 
-List diff1 = (sourceList-targetList)
-List diff2 = (targetList-sourceList)
-List diff3 = sourceList.intersect(targetList)
+Listdiff1=(sourceList-targetList)
+Listdiff2=(targetList-sourceList)
+Listdiff3=sourceList.intersect(targetList)
 
-println "Total in sourceList: ${sourceList.size}"
-println "Total in targetList: ${targetList.size}"
-
-
-println "**** Items in sourceList not in targetList list: ${diff1.size} ****"
-diff1.each { println "${it} \n" }
+println"TotalinsourceList:${sourceList.size}"
+println"TotalintargetList:${targetList.size}"
 
 
-println "**** Items in targetList not in sourceList list: ${diff2.size} ****"
-diff2.each { println "${it} \n" }
+println"****ItemsinsourceListnotintargetListlist:${diff1.size}****"
+diff1.each{println"${it}\n"}
+
+
+println"****ItemsintargetListnotinsourceListlist:${diff2.size}****"
+diff2.each{println"${it}\n"}
 
 
 
-println "***** common items *****"
-diff3.each { println "${it} \n" }
+println"*****commonitems*****"
+diff3.each{println"${it}\n"}
