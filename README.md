7dd56297711cbd5c5e1a7a59968381a39e2931a1
Ali Imtiaz <alimtiaz@deloitte.com.au>
2018-11-02 10:51:16 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# compare-list
+
+Simple Groovy script to compare 2 files. 
+
+
+## Running
+Populate the input-file1.txt and input-file2.txt and run: `groovy compareList.groovy`
+
---
+++ compareList.groovy
@@ -0,0 +1,29 @@
+def sourceList = []
+new File( 'input-file1.txt' ).eachLine { line ->
+    sourceList << line
+}
+
+def targetList = []
+new File( 'input-file2.txt' ).eachLine { line ->
+    targetList << line
+}
+
+List diff1 = (sourceList-targetList)
+List diff2 = (targetList-sourceList)
+List diff3 = sourceList.intersect(targetList)
+
+println "Total in sourceList: ${sourceList.size}"
+println "Total in targetList: ${targetList.size}"
+
+
+println "**** Items in sourceList not in targetList list: ${diff1.size} ****"
+diff1.each { println "${it} \n" }
+
+
+println "**** Items in targetList not in sourceList list: ${diff2.size} ****"
+diff2.each { println "${it} \n" }
+
+
+
+println "***** common items *****"
+diff3.each { println "${it} \n" }
